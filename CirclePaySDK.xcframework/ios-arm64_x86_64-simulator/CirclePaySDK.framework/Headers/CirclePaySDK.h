1f5e0e2e44cbbee664110f64e7d9a556cb52eef0
a <mahmoudallam@circlepay.ai>
2022-06-13 17:13:18 +0200

    first commit

---
+++ CirclePaySDK.xcframework/ios-arm64_x86_64-simulator/CirclePaySDK.framework/Headers/CirclePaySDK.h
@@ -0,0 +1,18 @@
+//
+//  CirclePaySDK.h
+//  CirclePaySDK
+//
+//  Created by Alchemist on 13/06/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CirclePaySDK.
+FOUNDATION_EXPORT double CirclePaySDKVersionNumber;
+
+//! Project version string for CirclePaySDK.
+FOUNDATION_EXPORT const unsigned char CirclePaySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CirclePaySDK/PublicHeader.h>
+
+
